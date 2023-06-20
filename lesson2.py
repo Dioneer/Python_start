9e4974888048f3ab4215f72632f634caa602750d
Dioneer <Gulkina_em@mail.ru>
2023-06-20 12:42:16 +0300

    lection 2

--- lesson2.py
+++ lesson2.py
@@ -26,3 +26,58 @@
 #     print(index)
 # else:
 #     print(result)
+
+# По данному целому неотрицательному n вычислите значение n!.
+# fact = int(input("Введите число: "))
+# result = 1
+# while fact > 0:
+#     result *= fact
+#     fact -= 1
+# print(result)
+
+# n = int(input('Enter number pls: '))
+# i = 1
+# fact = 1
+# while i <= n:
+#     fact, i = fact*i, i+1
+# print(fact)
+
+# fibo = int(input("Введите число: "))
+# tepm1 = 0
+# tepm2 = 1
+# index = 2
+# while tepm2 < fibo:
+#     tepm1, tepm2, index = tepm2, tepm1+tepm2, index + 1
+# if tepm2 == fibo:
+#     print(index)
+# else:
+#     print(-1)
+
+# Пользователь вводит число N – общее количество рассматриваемых дней (1 ≤ N ≤ 100). В следующих строках располагается N целых чисел.
+# Каждое число – среднесуточная температура в соответствующий день. Температуры – целые числа и лежат в диапазоне от –50 до 50
+# n = int(input("Введите число дней: "))
+# counter = 0
+# maxValue = 0
+# for number in range(n):
+#     newNumber = int(input("Введите число: "))
+#     if newNumber > 0:
+#         counter += 1
+#     elif counter > maxValue:
+#         maxValue = counter
+#     elif newNumber < 0:
+#         counter = 0
+# print(maxValue)
+
+# Пользователь вводит одно число N – количество арбузов. Вторая строка содержит N чисел, записанных на новой строчке каждое. Здесь каждое число – это масса соответствующего арбуза
+# n = int(input("Введите количество арбузов: "))
+# x = int(input("Введите массу арбуза - "))
+# minWeigt = x
+# maxWeight = x
+# for i in range(n-1):
+#     x = int(input("Введите массу арбуза - "))
+#     if x<minWeigt:
+#         minWeigt = x
+#     if x>maxWeight:
+#         maxWeight = x
+
+# print(minWeigt, maxWeight)
